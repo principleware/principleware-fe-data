1251a9531e978406391c9dd6b3bc458f64be38ea
Xiaolong Tang <xxlongtang@gmail.com>
2017-12-20 17:13:55 -0500

    Database schema definition

--- host/relational/interfaces.ts
+++ host/relational/interfaces.ts
@@ -5,3 +5,21 @@ export interface IModelLike {
     getForeignModel(foreignKey: string): IModelLike;
     hasAnyReference(): boolean;
 }
+
+export interface IRelationDatabaseSchema {
+    leafTables: {
+        [key: string]: {
+            relations?: {
+                [key: string]: string
+            }
+        }
+    };
+
+    assocations: {
+        [key: string]: {
+            relations: {
+                [key: string]: string
+            }
+        }
+    };
+}
