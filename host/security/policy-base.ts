8d5164f206b15278e2e62da2b933bd30e1345a20
Xiaolong Tang <xxlongtang@gmail.com>
2017-12-21 05:41:31 -0500

    oauth

---
+++ host/security/policy-base.ts
@@ -0,0 +1,62 @@
+/**
+ * @fileOverview
+ * A base class for defining security plicies. 
+ */
+
+import * as dependencies from 'principleware-fe-dependencies';
+
+import { lift } from 'principleware-fe-utilities/src/promise/monadic-operations';
+
+import { IPolicyCtorOptions } from './interfaces';
+
+const _ = dependencies.underscore;
+
+export abstract class PolicyBase {
+
+    protected url: string;
+    protected token: string;
+
+    constructor(settings: IPolicyCtorOptions) {
+        this.url = settings.url;
+        this.token = '';
+    }
+
+    abstract getTokenInternal(): PromiseLike<string>;
+
+    abstract applyTo(options: any): void;
+
+    abstract isExpired(): boolean;
+
+    abstract readFrom(settings: {});
+
+    abstract persistent(): any;
+
+    abstract applyToV2(options: any): void;
+
+    abstract applyToV3(options: any): void;
+
+
+    /**
+     * The interface for retrieving the token from a remote server.
+     * This method internally dispatches the call to another method
+     * and cache the token. 
+     */
+    getTokenP(): PromiseLike<string> {
+        if (!_.isEmpty(this.token) && !this.isExpired()) {
+            return lift(this.token, null);
+        }
+
+        return this.getTokenInternal()
+            .then((token) => {
+                return this.token = token;
+            });
+    }
+
+    /**
+     * Reset the security policy, e.g.,
+     * removing established token.
+     */
+    reset() {
+        this.token = '';
+    }
+}
