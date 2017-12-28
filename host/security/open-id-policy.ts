74eb9c5adaf76c7930c497f12f66bf477a124467
Xiaolong Tang <xxlongtang@gmail.com>
2017-12-28 14:03:57 -0500

    open id policy

---
+++ host/security/open-id-policy.ts
@@ -0,0 +1,49 @@
+﻿/**
+ * @fileOverview
+ * OpenID token policy, built upon OAuth2 token policy
+ */
+
+import {
+    IOpenIDToken
+} from './interfaces';
+
+import { OAuthTokenPolicy, adaptToOAuthToken } from './oauth-token-policy';
+
+export function adaptToOpenIDToken(data): IOpenIDToken {
+    data = data || {};
+
+    const r = adaptToOAuthToken(data);
+    return { ...r, openId: data.openId || '' };
+}
+
+export class OpenIDPolicy extends OAuthTokenPolicy {
+
+    protected openId: string;
+
+    constructor() {
+        super({
+            url: 'dummy',
+            clientId: 'dummy',
+            clientSecret: 'dummy',
+            scope: 'all'
+        });
+        this.openId = '';
+    }
+
+    /**
+     * Returns the necessary information for peristence.
+     */
+    persistent(): IOpenIDToken {
+        const r = super.persistent();
+        return { ...r, openId: this.openId };
+    }
+
+    /**
+     * Reads credential from the given settings.
+     */
+    readFrom(settings: IOpenIDToken) {
+        super.readFrom(settings);
+        this.openId = settings.openId;
+        return this;
+    }
+}
