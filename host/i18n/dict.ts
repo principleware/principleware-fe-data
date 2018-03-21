6d50ea24c2e6f703bcc38c468e5d1ad80e2cc6da
Xiaolong Tang <xxlongtang@gmail.com>
2018-03-21 19:00:10 -0400

    i18n dict

---
+++ host/i18n/dict.ts
@@ -0,0 +1,48 @@
+/**
+ * @fileOverview
+ * Provides i18n service. This module is designed as 
+ * a delegate of the tinymce I18n service.
+ * @author Xiaolong Tang <xxlongtang@gmail.com>
+ * @license Copyright @me
+ */
+import * as _i18n from 'principleware-tinymce-tailor/src/util/I18n';
+
+export class I18n {
+
+    /**
+     * Trnsaltes a given text. If the given text
+     * is missing in the dictionary, use the given default value.
+     * @function translate
+     * @param {String} text A text to be translated.
+     * @param {String} defaultText The default value.
+     * @returns {String} The translation for the given text. 
+     */
+    static translate(text: string, defaultText: string) {
+        const value = _i18n.translate(text);
+        if (value === text && defaultText) {
+            return defaultText;
+        }
+        return value;
+    }
+
+    /**
+     * Removes unused languages to release memory.
+     * @function recycleOthers
+     * @param {String} code The language code which should not released.
+     */
+    static recycleOthers(code: string) {
+        const data = _i18n.data;
+        const recycleList = [];
+        for (let key in data) {
+            // skip loop if the property is from prototype
+            if (data.hasOwnProperty(key) && key !== code) {
+                recycleList.push(key);
+            }
+        }
+        /*jslint plusplus: true */
+        for (let i = 0; i < recycleList.length; i++) {
+            let k = recycleList[i];
+            delete data[k];
+        }
+    }
+}
