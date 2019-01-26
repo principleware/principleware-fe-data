59be6beb3775b98352434397ac8e648a61a712e8
Tom <xiaolong@cse.tamu.edu>
2019-01-26 15:58:25 -0500

    Fix scripts for doc

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ const { src, dest, parallel } = require('gulp');
 const typedoc = require("gulp-typedoc");
 
 function doc() {
-    return src(["src/**/*.ts"])
+    return src(["projects/polpware/fe-data/src/lib/**/*.ts"])
         .pipe(typedoc({
             name: "Polpware typescript data (3.0.1)",            
             out: "docs/",            
             
             module: "commonjs",
             target: "es5",
+
+            exclude: "projects/polpware/fe-data/src/lib/**/*.spec.ts",                        
 
             experimentalDecorators: true,
             excludePrivate: true,
